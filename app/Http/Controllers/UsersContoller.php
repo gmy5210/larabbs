002e71d80a3efa3dd8498910211d47cad9a4f13c
gmy5210 <294182345@qq.com>
2018-12-22 08:16:41 +0000

    Only authorized user can edit user profile

--- app/Http/Controllers/UsersContoller.php
+++ app/Http/Controllers/UsersContoller.php
@@ -9,6 +9,15 @@ use App\Http\Requests\UserRequest;
 
 class UsersContoller extends Controller
 {
+
+    public function __construct()
+    {
+
+        $this->middleware('auth', ['except' => ['show']]);
+
+    }
+
+
     public function show(User $user)
     {
 
@@ -19,13 +28,16 @@ class UsersContoller extends Controller
     public function edit(User $user)
     {
 
+        $this->authorize('update', $user);
+
         return view('users.edit', compact('user'));
 
     }
 
     public function update(UserRequest $request, ImageUploadHandler $uploader,User $user)
     {
-
+        
+        $this->authorize('update', $user);
 
         $data = $request->all();
 
